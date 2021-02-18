0ae8a21b8d655fab8ebbcb2d1a994232cf24ab8a
Icy Defiance <icydefiance@gmail.com>
2021-02-17 23:06:20 -0500

    Update udp.cpp

--- src/udp.cpp
+++ src/udp.cpp
@@ -4,13 +4,29 @@
 #include <Ws2tcpip.h>
 #else
 #include <arpa/inet.h>
-#include <netinet/in.h>
+#include <errno.h>
+#include <netinet/ip.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #endif // #ifdef _WIN32
+#include <cstring>
 #include <thread>
 
 using namespace std;
+
+void handleError() {
+#ifdef _WIN32
+	wprintf(L"failed with error %d\n", WSAGetLastError());
+	wchar_t* s = NULL;
+	FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL, WSAGetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR)&s, 0, NULL);
+	printf("%S\n", s);
+	LocalFree(s);
+#else
+	printf("%s\n", strerror(errno));
+#endif // #ifdef _WIN32
+	exit(1);
+}
 
 int main(int argc, char* argv[]) {
 #ifdef _WIN32
@@ -19,20 +35,21 @@ int main(int argc, char* argv[]) {
 #endif // #ifdef _WIN32
 
 	thread recvThread([] {
-		SOCKET in = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+		auto in = socket(PF_INET, SOCK_RAW, IPPROTO_UDP);
+		if (in == -1) { handleError(); }
 
-		sockaddr_in addr;
-		addr.sin_family = AF_INET;
-		addr.sin_port = htons(25565);
-		addr.sin_addr.s_addr = htonl(INADDR_ANY);
-		bind(in, (SOCKADDR*)&addr, sizeof(addr));
+		sockaddr_in src;
+		src.sin_family = AF_INET;
+		src.sin_port = htons(0);
+		inet_pton(AF_INET, "127.0.0.1", &src.sin_addr.s_addr);
+		bind(in, (sockaddr*)&src, sizeof(src));
 
 		char buf[1024];
 		sockaddr_in from;
-		int fromSize = sizeof(from);
-		auto len = recvfrom(in, buf, 1024, 0, (SOCKADDR*)&from, &fromSize);
-		if (len == SOCKET_ERROR) { handleError(); }
-		printf("received %i bytes", len);
+		socklen_t fromSize = sizeof(from);
+		auto len = recvfrom(in, buf, sizeof(buf), 0, (sockaddr*)&from, &fromSize);
+		if (len == -1) { handleError(); }
+		printf("received %li bytes\n", len);
 
 #ifdef _WIN32
 		closesocket(in);
@@ -41,7 +58,9 @@ int main(int argc, char* argv[]) {
 #endif // #ifdef _WIN32
 	});
 
-	SOCKET out = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	this_thread::sleep_for(1000ms);
+
+	auto out = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
 	sockaddr_in dest;
 	dest.sin_family = AF_INET;
@@ -61,16 +80,5 @@ int main(int argc, char* argv[]) {
 
 #ifdef _WIN32
 	WSACleanup();
-#endif // #ifdef _WIN32
-}
-
-void handleError() {
-#ifdef _WIN32
-	wprintf(L"recvfrom failed with error %d\n", WSAGetLastError());
-	wchar_t* s = NULL;
-	FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-		NULL, WSAGetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR)&s, 0, NULL);
-	printf("%S\n", s);
-	LocalFree(s);
 #endif // #ifdef _WIN32
 }
